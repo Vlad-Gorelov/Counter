d86933d25f6119960580c73549c24309f4b45a35
Vladislav Gorelov <vsg@yandex.ru>
2023-03-25 23:17:14 +0300

    Сделали счётчик, соблюдая условия задачи: 1. Создали две кнопки: с икнокой «+» — красного цвета, с иконкой «-» — синего цвета.
    2. Создали кнопку обнуления с подходящей иконкой, которая при нажатии обнуляет значение счётчика.
    
    3. Создали текстовое поле UITextView, в котором отражена история изменения значения счётчика следующего формата:
    - при старте приложения — «История изменений:»
    - при успешном нажатии на кнопку «+» или «-» — «[дата и время]: значение изменено на -1/+1»
    - при сбросе: — «[дата и время]: значение сброшено»
    - при попытке уменьшить значение счётчика ниже 0 — «[дата и время]: попытка уменьшить значение счётчика ниже 0»
    
    4. Убедились, что:
    	- Приложение запускается и не крашится;
    	- При старте приложение у лейбла показан текст «0», а текст в текстовом поле соответствует описанию пункта 3 условия задачи;
    	- При нажатии на кнопку «+» или «-» значение счётчика меняется на -1 (если не было равно 0) или +1. Значение счётчика обнуляется в тексте лейбла, а в текстовом поле добавляется строка (согласно пункту 3 условия задачи);
    	- При нажатии на кнопку обнуления значение счётчика обнуляется в тексте лейбла, а в текстовом поле добавляется строка (согласно пункту 3 условия задачи).
    	- Кнопки имеют соответствующие условиям задачи цвета и иконки;
    	- Текстовое поле скроллится, но заблокировано для редактирования пользователем.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var setToZeroButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var progressTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "0"
+        plusButton.tintColor = .red
+        minusButton.tintColor = .blue
+        setToZeroButton.tintColor = .gray
+        progressTextView.layer.cornerRadius = 12
+        progressTextView.contentInsetAdjustmentBehavior = .always
+
+    }
+
+    @IBAction func counterPlus(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        addProgressText("+1")
+    }
+
+    @IBAction func counterMinus(_ sender: Any) {
+        if counter > 0 {
+        counter -= 1
+        counterLabel.text = "\(counter)"
+        addProgressText("-1")
+        scrollToBottom()
+        } else {
+            progressTextView.text.append("\(getCurrentDateTime())Попытка уменьшить значение счётчика ниже 0 \n")
+            scrollToBottom()
+        }
+    }
+
+    @IBAction func counterToZero(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+        progressTextView.text.append("\(getCurrentDateTime())Значение сброшено \n")
+        scrollToBottom()
+    }
+
+    private var counter: Int = 0
+ 
+    private func addProgressText(_ change: String) {
+            let text = "\(getCurrentDateTime())Значение изменено на \(change)\n"
+        let currentText = progressTextView.text ?? ""
+            progressTextView.text = "\(currentText)\(text)"
+        scrollToBottom()
+        }
+
+    private func scrollToBottom() { //добавляет автоскролл, когда текст выходит за границы обозначенной области
+        if progressTextView.text.isEmpty { return }
+        let bottomRange = NSRange(location: progressTextView.text.count - 1, length: 1)
+        progressTextView.scrollRangeToVisible(bottomRange)
+    }
+
+    func getCurrentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "\ndd-MM-yyyy HH:mm:ss \n"
+        return formatter.string(from: Date())
     }
 
 
 }
+
 
